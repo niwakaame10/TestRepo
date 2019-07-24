90b6c8005c3935539016e72e7007b109ee96a0f0
福田晃希 <kouki@fukudaakiraki-no-MacBook-Air.local>
2019-07-24 20:54:17 +0900

    お試し

--- default.js
+++ default.js
@@ -13,56 +13,66 @@ var personAllay = [];
 var personInfo = {name,count}
 
 var array = [];
-var count = 0;
+var currentCount = 0;
+
+var currentName = "";
 
 // 入力ボタン押下
 function clickbutton() {
   var name = document.getElementById("name").value;
 
-/*
+
   //入力された名前が存在するかチェック
   foreach(info in personAllay){
 
     // 以前入力された名前の場合
     if(name == info.name){
       info.count = info.count + 1;
-      //break;
+      currentCount = info.count;
+      break;
     }
     // 初めて入力された名前の場合
     else{
       personInfo.name = name;
       personInfo.count = 1;
       personAllay.push(personInfo);
+      currentCount = 1;
+      break;
     }
   }
-*/
 
+/*
  for(var i = 0;i <= array.length;i++){
    if(name == array[i]){
      count = count + 1;
      break;
    }
  }
+*/
 
+/*
   personInfo.name = "gorira";
   personInfo.count = 1;
 
   personAllay.push(personInfo);
+*/
 
-  array.push(name);
-  if(count === 0){
+  //array.push(name);
+
+
+  if(currentCount === 0){
     document.getElementById("form-text").innerHTML = "はじめまして " + name + " さん！";
   }
 
-  if(count === 1){
+  if(currentCount === 1){
     document.getElementById("form-text").innerHTML = "また会ったね " + name + " さん！";
   }
 
-  if(count === 2){
+  if(currentCount === 2){
     document.getElementById("form-text").innerHTML = "しつこいね " + name + " さん！";
   }
 
-  if(2 < count){
+  if(2 < currentCount){
     document.getElementById("form-text").innerHTML = "呆れたよ " + name + " さん！";
   }
 
